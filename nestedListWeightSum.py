c3fd3e3072e274c6dd15e86bf864d8c679cbb349
Abdullah Shahid <abdullahshahid20022002@gmail.com>
2022-06-04 16:15:41 -0400

    added remove Duplicates from Sorted List

---
+++ nestedListWeightSum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def depthSum(self, nestedList):
+        sum = dfs(nestedList, 1)
+        return sum
+
+    def dfs(self, nestedlist, depth):
+        sum = 0
+        if (len(nestedlist == 0)):
+            return 0
+        for item in nestedlist:
+            if item.isInteger():
+                sum += depth*item.getInteger()
+            else:
+                sum+=self.dfs(item.getList(), depth+1)
+        return sum
