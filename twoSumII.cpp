cf992cea89031d141d822a8ce7e33a729bf68498
Abdullah Shahid <abdullahshahid20022002@gmail.com>
2022-11-14 22:58:40 -0500

    Fixed twoSum II

--- twoSumII.cpp
+++ twoSumII.cpp
@@ -1,20 +1,27 @@
-class Solution {
+// Two pointer method
+class Solution
+{
 public:
-    vector<int> twoSum(vector<int>& numbers, int target) {
-    int i = 0;
-    int j = numbers.size() - 1;
+    vector<int> twoSum(vector<int> &numbers, int target)
+    {
+        int i = 0;
+        int j = numbers.size() - 1;
 
-    while (i < j) {
-        if (numbers[i] + numbers[j] == target) {
-            break;
+        while (i < j)
+        {
+            if (numbers[i] + numbers[j] == target)
+            {
+                break;
+            }
+            else if (numbers[i] + numbers[j] < target)
+            {
+                i++;
+            }
+            else
+            {
+                j--;
+            }
         }
-        else if(numbers[i] + numbers[j] < target) {
-            i++;
-        }
-        else {
-            j--;
-        }
+        return {i + 1, j + 1};
     }
-    return {i+1, j+1};
-}
 };
