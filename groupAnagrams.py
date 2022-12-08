9cc80771c7de5fbc91de3c6e1b82a8053bcdfca5
Abdullah Shahid <abdullahshahid20022002@gmail.com>
2022-12-08 17:42:26 -0500

    Added comment

--- groupAnagrams.py
+++ groupAnagrams.py
@@ -1,12 +1,14 @@
 class Solution:     
     def groupAnagrams(self, strs):
         anagrams = defaultdict(list)
-
+        
         for s in strs:
+            # use count as a hashmap to track the number of times a letter occurs.
             count = [0] * 26
             
             for c in s:
                 count[ord(c) - ord("a")] += 1
+            # store all strings with the same count hashmap in dict
             anagrams[tuple(count)].append(s)
-
+        
         return anagrams.values()
